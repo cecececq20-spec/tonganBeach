010c189cfae36aee5d81f774c6646635747921b0
cecececq20-spec <cecececq20@gmail.com>
2026-04-30 16:57:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,53 +5,75 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { 
-    cors: { origin: "*" } 
-});
+const io = new Server(server, { cors: { origin: "*" } });
 
-// ЧИТАЕТ ФАЙЛЫ ИЗ КОРНЯ (твоя структура!)
 app.use(express.static(__dirname));
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
-const gameState = { players: {} };
+const rooms = {};  // {roomId: {players: {}, items: []}}
 
 io.on('connection', (socket) => {
-    console.log(`Игрок ${socket.id} подключился`);
+    console.log(`Игрок ${socket.id}`);
     
-    socket.on('join', (data) => {
-        gameState.players[socket.id] = { 
-            id: socket.id, 
-            x: 100, y: 100, 
-            score: 0, 
-            dir: 'down',
-            ...data 
+    // Создать/присоединиться к комнате
+    socket.on('lobbyJoin', (data) => {
+        const { roomId, playerName } = data;
+        if (!rooms[roomId]) rooms[roomId] = { players: {}, started: false };
+        
+        socket.join(roomId);
+        rooms[roomId].players[socket.id] = { 
+            id: socket.id, name: playerName, 
+            x: 100, y: 100, score: 0, ready: false 
         };
-        io.emit('playersUpdate', Object.values(gameState.players));
+        
+        io.to(roomId).emit('lobbyUpdate', {
+            roomId,
+            players: Object.values(rooms[roomId].players),
+            started: rooms[roomId].started
+        });
     });
     
-    socket.on('move', (data) => {
-        if (gameState.players[socket.id]) {
-            gameState.players[socket.id] = { 
-                ...gameState.players[socket.id], 
-                ...data 
-            };
-            socket.broadcast.emit('playerMoved', gameState.players[socket.id]);
+    socket.on('toggleReady', (roomId) => {
+        if (rooms[roomId]?.players[socket.id]) {
+            rooms[roomId].players[socket.id].ready = !rooms[roomId].players[socket.id].ready;
+            io.to(roomId).emit('lobbyUpdate', {
+                roomId,
+                players: Object.values(rooms[roomId].players),
+                started: rooms[roomId].started
+            });
         }
     });
     
-    socket.on('pickup', (data) => {
-        io.emit('itemPicked', data);
+    socket.on('startGame', (roomId) => {
+        if (rooms[roomId] && Object.keys(rooms[roomId].players).length >= 2) {
+            rooms[roomId].started = true;
+            io.to(roomId).emit('gameStart', roomId);
+        }
+    });
+    
+    socket.on('move', (data) => {
+        const roomId = Array.from(socket.rooms)[1];
+        if (rooms[roomId]?.players[socket.id]) {
+            rooms[roomId].players[socket.id] = { 
+                ...rooms[roomId].players[socket.id], ...data 
+            };
+            socket.to(roomId).emit('playerMoved', data);
+        }
     });
     
     socket.on('disconnect', () => {
-        delete gameState.players[socket.id];
-        io.emit('playersUpdate', Object.values(gameState.players));
+        for (let roomId in rooms) {
+            if (rooms[roomId].players[socket.id]) {
+                delete rooms[roomId].players[socket.id];
+                io.to(roomId).emit('lobbyUpdate', {
+                    roomId,
+                    players: Object.values(rooms[roomId].players),
+                    started: rooms[roomId].started
+                });
+                break;
+            }
+        }
     });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Tongan Beach MP на порту ${PORT}`);
-});
+server.listen(process.env.PORT || 3000);
